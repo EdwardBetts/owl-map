2e8ff40d3d35e299a67050a3530c54c5e3b7a383
Edward Betts <edward@4angle.com>
2023-05-14 11:06:08 +0200

    Update

---
+++ matcher/__init__.py
@@ -0,0 +1,13 @@
+"""Match OSM and Wikidata items."""
+
+
+CallParams = dict[str, str | int]
+
+user_agent = (
+    "osm-wikidata/0.1 (https://github.com/EdwardBetts/osm-wikidata; edward@4angle.com)"
+)
+
+
+def user_agent_headers() -> dict[str, str]:
+    """User-Agent headers."""
+    return {"User-Agent": user_agent}
