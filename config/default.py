0b48f932cbc3dc91dfe0911bcd7a4777b07065b8
Edward Betts <edward@4angle.com>
2023-05-14 10:49:29 +0200

    Add sample config

---
+++ config/default.py
@@ -0,0 +1,25 @@
+"""Sample config."""
+
+
+ID_TAGGING_SCHEMA_DIR = "/var/lib/data/id-tagging-schema"
+ID_PRESET_DIR = "/var/lib/data/id-tagging-schema/data/presets/"
+GEOIP_DATA = "/var/lib/data/GeoIP/GeoIPCity.dat"
+GEOLITE2 = "/var/lib/data/GeoLite2/GeoLite2-City.mmdb"
+
+CLIENT_KEY = ""
+CLIENT_SECRET = ""
+
+SECRET_KEY = ""
+
+DEFAULT_COMMENT = "+wikidata"
+
+ADMIN_NAME = ""
+ADMIN_EMAIL = ""
+ADMINS = [ADMIN_EMAIL]
+
+SMTP_HOST = "localhost"
+MAIL_FROM = "osm-wikidata@localhost"
+
+ERROR_MAIL = True
+
+PROPAGATE_EXCEPTIONS = False
