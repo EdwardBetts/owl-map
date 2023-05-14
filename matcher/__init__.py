d1e5a3122598b91879c69135f7930df90950f3b4
Edward Betts <edward@4angle.com>
2023-05-14 10:33:18 +0000

    Update version in User-Agent header

--- matcher/__init__.py
+++ matcher/__init__.py
@@ -4,7 +4,9 @@
 CallParams = dict[str, str | int]
 
 user_agent = (
-    "osm-wikidata/0.1 (https://github.com/EdwardBetts/osm-wikidata; edward@4angle.com)"
+    "osm-wikidata/0.2"
+    + " (https://github.com/EdwardBetts/osm-wikidata;"
+    + " edward@4angle.com)"
 )
 
 
