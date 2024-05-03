b0a53645e652967c24952ffa8bc858e1d09f4e73
Edward Betts <edward@4angle.com>
2024-05-03 18:51:22 +0000

    Switch from snowpack to vite for frontend tooling
    Closes: #3

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  define: { 'process.env.NODE_ENV': '"production"' },
+  build: {
+    target: 'esnext',
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.js'),
+      name: 'OWL',
+      fileName: (format) => `owl.${format}.js`
+    },
+  },
+    /* rollupOptions: {
+      external: ['vue', 'leaflet'],
+	  output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+          leaflet: 'L',
+        }
+      }
+	}, */
+  // },
+})
